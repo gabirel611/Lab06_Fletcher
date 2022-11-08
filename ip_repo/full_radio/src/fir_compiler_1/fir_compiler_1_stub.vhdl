65eb0774cad2b90c58457c4560adb75ad8b95a97
gabirel611 <gfletch4@jh.edu>
2022-11-07 21:08:54 -0500

    Initial commit

---
+++ ip_repo/full_radio/src/fir_compiler_1/fir_compiler_1_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+-- Date        : Mon Nov  7 19:55:18 2022
+-- Host        : GabrielPC running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               d:/GradSchool/VHDL_Lab/Lab_6/lab06_git_orig/radio_periph_lab/ip_repo/full_radio/src/fir_compiler_1/fir_compiler_1_stub.vhdl
+-- Design      : fir_compiler_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fir_compiler_1 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_data_tvalid : in STD_LOGIC;
+    s_axis_data_tready : out STD_LOGIC;
+    s_axis_data_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end fir_compiler_1;
+
+architecture stub of fir_compiler_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_data_tvalid,s_axis_data_tready,s_axis_data_tdata[15:0],m_axis_data_tvalid,m_axis_data_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fir_compiler_v7_2_18,Vivado 2022.1";
+begin
+end;
